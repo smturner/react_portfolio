f417e790832efd6b693549de2873715c49bcb40e
Sarah Turner <turnersarah228@gmail.com>
2022-05-23 21:11:32 -0500

    changed background colors

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,13 +14,14 @@ import './Navbar.css';
 // }
 function Navbar({ currentPage, handlePageChange }) {
     return (
-        <nav className="navbar navbar-expand-lg" >
+        <nav className="navbar navbar-expand-lg" id="navbarMain" >
             <div className="container-fluid">
                 <a className=" navbar-brand " id="name" href="#">Sarah Turner</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">   <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navLinks" >
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">   
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navLinks" >
                     <li className="nav-item">
                         <a  href="#about" onClick={() => handlePageChange('About')} className = {currentPage === 'About' ? 'nav-link active' : 'nav-link'}> About Me</a>        
                          </li>
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import "./About.css"
 
 const About = () => {
     return (
-        <p>Hello</p>
+        <section className="aboutMe">        
+            <div className="container my-auto">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                    <h1 className="title text-center">About Me</h1>
+                    <hr className="my-4" />
+                        <p>
+                            I am a former teacher of 5 years, expanding my skills and experience to make an exciting career change. I have resently graduated from a 12 week full stack boot camp at the University of Minnesota where I learned a variety of languages that I am ready to put to use in my next job.
+                        <br />
+                        <br />
+                            I have spent much of my professional career collaborating with my peers to develop resources that streamline and enhance learning, and utilizing my creativity and problem solving skills to tackle challenging situations. I am someone who works hard, sets goals, and pursues them to the best of my abilitiy. Being proficient in these skills, I believe, will have a positive impact on my future employeer and co-workers. I plan to do full-stack development with an emphasis on backend databases, as well as market my skills for non-profits and small buisnesses. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
